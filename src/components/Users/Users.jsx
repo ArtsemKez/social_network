2653aedd9af64bfbba4db35ceddc49f45be0923d
ArtsemKez <artsemkez@yandex.ru>
2020-04-07 00:28:48 +0300

    Refactoring Users.jsx(componentDidMount)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,15 +5,12 @@ import userPhoto from '../../assets/images/user.jpg'
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    this.props.setUsers(response.data.items);
-                }
-            )
-        }
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            }
+        )
     }
 
 
