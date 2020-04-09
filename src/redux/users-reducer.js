907c3eb4d910093d446662e33a6f3db25e3deaa2
ArtsemKez <artsemkez@yandex.ru>
2020-04-10 02:04:32 +0300

    Add pagination of users page

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,15 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGES';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
-    users:[]
+    users:[],
+    pageSize: 50,
+    totalUsersCount: 0,
+    currentPage: 1
+
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -19,6 +25,7 @@ const usersReducer = (state = initialState, action) => {
                     return u;
                 })
             }
+
         case UNFOLLOW:
             return {
                 ...state,
@@ -29,8 +36,16 @@ const usersReducer = (state = initialState, action) => {
                     return u;
                 })
             }
+
         case SET_USERS:
-            return { ...state, users: [...state.users, ...action.users] }
+            return { ...state, users: action.users }
+
+        case SET_CURRENT_PAGE:
+            return { ...state, currentPage: action.currentPage }
+
+        case SET_TOTAL_USERS_COUNT:
+            return { ...state, totalUsersCount: action.count}
+
         default:
             return state;
     }
@@ -40,5 +55,7 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCountAC = (totalUsersCount) => ({type:SET_TOTAL_USERS_COUNT, count: totalUsersCount})
 
 export default usersReducer;
