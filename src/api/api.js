05ccaedba64f9ffce9c276b6a25a2f40f94c0048
ArtsemKez <artsemkez@yandex.ru>
2020-04-25 20:30:49 +0300

    Add api.js(DAL)

---
+++ src/api/api.js
@@ -0,0 +1,27 @@
+import * as Axios from "axios";
+
+export const instance = Axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: { "API-KEY": "0a4d5e42-bfac-4525-abce-dd128f2c8444" }
+
+})
+
+export const usersAPI = {
+    getUsers (currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            })
+    },
+}
+
+export const profileAPI = {
+    getProfile (userId) {
+        return instance.get('profile/' + userId)
+        .then(response => {
+            return response.data;
+        })
+    }
+}
+
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from '../../../common/Preloader/Preloader';
+import userPhoto from '../../../assets/images/user.jpg';
+
 
 const ProfileInfo = (props) => {
   if (!props.profile) { return <Preloader /> }
@@ -9,7 +11,7 @@ const ProfileInfo = (props) => {
       <img src='https://securenews.ru/wp-content/uploads/2016/12/Network.jpg' />
     </div>
     <div className={s.DescriptionBlock} >
-      <img src={props.profile.photos.large} />
+      <img src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto} />
       <div>{props.profile.fullName}</div>
       <div>{props.profile.aboutMe}</div>
     </div>
