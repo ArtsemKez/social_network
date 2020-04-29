efff19acb4824ab6297402a4539c39718f160bc0
ArtsemKez <artsemkez@yandex.ru>
2020-04-30 01:39:31 +0300

    componentDidUpdate

--- src/api/api.js
+++ src/api/api.js
@@ -15,7 +15,7 @@ export const usersAPI = {
 export const profileAPI = {
     getProfile (userId) {return instance.get(`profile/` + userId)},
     getStatus (userId) {return instance.get(`profile/status/`+ userId)},
-    updateStatus (status) {return instance.put(`profile/status/`, {status})}
+    updateStatus (status) {return instance.put(`profile/status`, {status})}
 }
 
 export const authAPI = {
