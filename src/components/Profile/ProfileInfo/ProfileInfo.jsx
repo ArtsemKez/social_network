20ef4e4e614030464f434fa2e13c46f179bbb7d4
ArtsemKez <artsemkez@yandex.ru>
2020-04-29 20:00:02 +0300

    Add ProfileStatus.jsx (setState, local state)

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from '../../../common/Preloader/Preloader';
 import userPhoto from '../../../assets/images/user.jpg';
+import ProfileStatus from './ProfileStatus';
 
 
 const ProfileInfo = (props) => {
   if (!props.profile) { return <Preloader /> }
   return <div className="content">
-    <div className={s.avaBlock}>
+    {/* <div className={s.avaBlock}>
       <img src='https://securenews.ru/wp-content/uploads/2016/12/Network.jpg' />
-    </div>
+    </div> */}
     <div className={s.DescriptionBlock} >
       <img src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto} />
+      <ProfileStatus status={"This is my status"}/>
       <div>{props.profile.fullName}</div>
       <div>{props.profile.aboutMe}</div>
     </div>
