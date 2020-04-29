6a798f2e1a38247a514cbf75938ed35c2a8d41c9
ArtsemKez <artsemkez@yandex.ru>
2020-04-30 01:09:56 +0300

    Debugging api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import * as Axios from "axios";
 
 export const instance = Axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
     withCredentials: true,
     headers: { "API-KEY": "0a4d5e42-bfac-4525-abce-dd128f2c8444" }
 })
@@ -13,11 +13,11 @@ export const usersAPI = {
 }
 
 export const profileAPI = {
-    getProfile (userId) {return instance.get('profile/' + userId)},
-    getStatus (userId) {return instance.get('profile/status/'+ userId)},
-    updateStatus (status) {return instance.put('profile/status/' + {status})}
+    getProfile (userId) {return instance.get(`profile/` + userId)},
+    getStatus (userId) {return instance.get(`profile/status/`+ userId)},
+    updateStatus (status) {return instance.put(`profile/status/`, {status})}
 }
 
 export const authAPI = {
-    me () {return instance.get('auth/me')}
+    me () {return instance.get(`auth/me`)}
 }
