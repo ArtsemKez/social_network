01413a820b2ebab35661abd381a41d84c8a02133
ArtsemKez <artsemkez@yandex.ru>
2020-05-01 01:35:12 +0300

    Install redux-form, refactoring login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import {Field, reduxForm} from "redux-form";
 
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    const onSubmit = (FormData) => {
+        console.log(FormData);
+    }
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
 }
+
+const LoginForm = (props) => {
+    return <form onSubmit={props.handleSubmit} >
+        <div>
+            <Field placeholder={"login"} component={'input'} name={'login'} />
+        </div>
+        <div>
+            <Field placeholder={"Password"} component={'input'} name={'password'} />
+        </div>
+        <div>
+            <Field type={"checkbox"} component={'input'} name={'rememberMy'} /> Remember my
+            </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+}
+
+const LoginReduxForm = reduxForm ({form: 'login'})(LoginForm)
+
 
 export default Login;
