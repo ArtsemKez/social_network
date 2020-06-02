2fc64e5aceb6c64694b90e0cb21cc1b0b15c9cb8
ArtsemKez <artsemkez@yandex.ru>
2020-06-02 03:51:43 +0300

    Updates

---
+++ precache-manifest.c762f3b8ced114d10562726ebaa0966b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a62699796fac8fea0645f1e8ab37f8",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "b45fb830dfb3784d11ac",
+    "url": "/social_network/static/css/4.03dd6edf.chunk.css"
+  },
+  {
+    "revision": "abef5fe24ff2b38cf73d",
+    "url": "/social_network/static/css/5.4ad0291b.chunk.css"
+  },
+  {
+    "revision": "6ab7ec7543e495c1b008",
+    "url": "/social_network/static/css/6.be5f71cf.chunk.css"
+  },
+  {
+    "revision": "bdbdfe1573ea51f3dc95",
+    "url": "/social_network/static/css/7.dace91b1.chunk.css"
+  },
+  {
+    "revision": "8dfc6c3359211a27f366",
+    "url": "/social_network/static/css/main.40de0dc6.chunk.css"
+  },
+  {
+    "revision": "d11ed137554309f94ebd",
+    "url": "/social_network/static/js/0.18492fc3.chunk.js"
+  },
+  {
+    "revision": "e42e931863244879fe76",
+    "url": "/social_network/static/js/3.5c05c9c4.chunk.js"
+  },
+  {
+    "revision": "b45fb830dfb3784d11ac",
+    "url": "/social_network/static/js/4.34a73082.chunk.js"
+  },
+  {
+    "revision": "abef5fe24ff2b38cf73d",
+    "url": "/social_network/static/js/5.d54990ba.chunk.js"
+  },
+  {
+    "revision": "6ab7ec7543e495c1b008",
+    "url": "/social_network/static/js/6.8666e2be.chunk.js"
+  },
+  {
+    "revision": "bdbdfe1573ea51f3dc95",
+    "url": "/social_network/static/js/7.f1164d0c.chunk.js"
+  },
+  {
+    "revision": "8dfc6c3359211a27f366",
+    "url": "/social_network/static/js/main.9acaff08.chunk.js"
+  },
+  {
+    "revision": "f8a618b07d869a4808e8",
+    "url": "/social_network/static/js/runtime~main.db2d34cf.js"
+  },
+  {
+    "revision": "0171b6bbd6a4ce93ddc61f36da178f29",
+    "url": "/social_network/static/media/preloader.0171b6bb.svg"
+  },
+  {
+    "revision": "6017f95622c5026f9a5fb1d56813ce3e",
+    "url": "/social_network/static/media/user.6017f956.jpg"
+  }
+]);
--- src/utils/validators/validators.js
+++
@@ -1,9 +0,0 @@
-export const required = (value) => {
-    if(value) return undefined;
-    return "Field as requaired"
-}
-
-export const maxLengthCreater = (maxLength) => (value) => {
-    if(value && value.length > maxLength) return `Max length is ${maxLength} simbols`;
-    return undefined;
-}
