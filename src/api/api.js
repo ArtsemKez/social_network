5ab8f329f3bd6b35f3a48b660a8fdb75755b52e6
ArtsemKez <artsemkez@yandex.ru>
2020-06-05 14:39:20 +0300

    Added change avatar

--- src/api/api.js
+++ src/api/api.js
@@ -15,7 +15,15 @@ export const usersAPI = {
 export const profileAPI = {
     getProfile (userId) {return instance.get(`profile/` + userId)},
     getStatus (userId) {return instance.get(`profile/status/`+ userId)},
-    updateStatus (status) {return instance.put(`profile/status`, {status})}
+    updateStatus (status) {return instance.put(`profile/status`, {status})},
+    savePhoto(photoFile) {
+        const formData = new FormData();
+        formData.append("image", photoFile)
+        return instance.put(`profile/photo`, formData, {
+            headers: {
+                'Content-Type': 'multypart/form-data'
+            }
+        })}
 }
 
 export const authAPI = {
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -6,10 +6,16 @@ import ProfileStatus from './ProfileStatus';
 
 
 const ProfileInfo = (props) => {
+  const onMainPhotoSelected = (e) => {
+    if (e.target.files.length) {
+      props.savePhoto(e.target.files[0]);
+    }
+  }
   if (!props.profile) { return <Preloader /> }
   return <div className="content">
     <div className={s.DescriptionBlock} >
       <img src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto} />
+      {props.isOwner && <input type={"file"} onChange={onMainPhotoSelected} />}
       <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
       <div>{props.profile.fullName}</div>
       <div>{props.profile.aboutMe}</div>
