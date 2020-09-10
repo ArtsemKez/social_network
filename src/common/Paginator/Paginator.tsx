7f465593dea74f9b50fd9fd8e306c3be04d7d363
Artsem <50839226+ArtsemKez@users.noreply.github.com>
2020-09-10 18:54:58 +0300

    Typed Paginator.tsx

--- src/common/Paginator/Paginator.tsx
+++ src/common/Paginator/Paginator.tsx
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 import styles from './Paginator.module.css';
 import cn from "classnames";
 
+type PropsType = {
+    totalItemsCount: number
+    pageSize: number
+    currentPage: number
+    onPageChanged: (pageNumber: number) => void
+    portionSize?: number
+}
 
-let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 20}) => {
+let Paginator: React.FC<PropsType> = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 20}) => {
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
-    let pages = [];
+    let pages: Array<number> = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
